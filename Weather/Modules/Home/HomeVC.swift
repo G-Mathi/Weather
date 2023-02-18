d00b635eab812a7dbeef3a81914796ef031bac2c
Gmathivathanan <gmathivathanan4@gmail.com>
2023-02-18 13:42:42 +0530

    Programmatical mode changes

---
+++ Weather/Modules/Home/HomeVC.swift
@@ -0,0 +1,16 @@
+//
+//  HomeVC.swift
+//  Weather
+//
+//  Created by dilax on 2023-02-18.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
