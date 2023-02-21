93e2e2098276afd6f55ef589c3c02f401e483cd1
Gmathivathanan <gmathivathanan4@gmail.com>
2023-02-21 11:37:04 +0530

    Model optional values set

--- Weather/Modules/Home/Model/Forecast.swift
+++ Weather/Modules/Home/Model/Forecast.swift
@@ -10,13 +10,13 @@ import Foundation
 // MARK: - Forecast
 
 struct Forecast: Codable {
-    var lat: Double
-    var lon: Double
-    var timezone: String
-    var timezoneOffset: Double
-    var current: WeatherInfo
-    var hourly: [WeatherInfo]
-    var daily: [WeatherInfoDaily]
+    var lat: Double?
+    var lon: Double?
+    var timezone: String?
+    var timezoneOffset: Double?
+    var current: WeatherInfo?
+    var hourly: [WeatherInfo]?
+    var daily: [WeatherInfoDaily]?
     
     enum CodingKeys: String, CodingKey {
         case lat, lon, timezone
@@ -28,10 +28,10 @@ struct Forecast: Codable {
 // MARK: - Weather Info
 
 struct WeatherInfo: Codable {
-    var dt: Int
-    var temp: Double
-    var feelsLike: Double
-    var weather: [WeatherDescription]
+    var dt: Int?
+    var temp: Double?
+    var feelsLike: Double?
+    var weather: [WeatherDescription]?
     
     enum CodingKeys: String, CodingKey {
         case dt, temp
@@ -43,12 +43,12 @@ struct WeatherInfo: Codable {
 // MARK: - Weather Info Daily
 
 struct WeatherInfoDaily: Codable {
-    var dt: Int
-    var sunrise: Int
-    var sunset: Int
-    var temp: Temp
-    var feelsLike: FeelsLike
-    var weather: [WeatherDescription]
+    var dt: Int?
+    var sunrise: Int?
+    var sunset: Int?
+    var temp: Temp?
+    var feelsLike: FeelsLike?
+    var weather: [WeatherDescription]?
     
     enum CodingKeys: String, CodingKey {
         case dt, sunrise, sunset, temp
@@ -60,24 +60,24 @@ struct WeatherInfoDaily: Codable {
 // MARK: - Other
 
 struct WeatherDescription: Codable {
-    var id: Int
-    var main: String
-    var description: String
-    var icon: String
+    var id: Int?
+    var main: String?
+    var description: String?
+    var icon: String?
 }
 
 struct Temp: Codable {
-    var day: Double
-    var min: Double
-    var max: Double
-    var night: Double
-    var eve: Double
-    var morn: Double
+    var day: Double?
+    var min: Double?
+    var max: Double?
+    var night: Double?
+    var eve: Double?
+    var morn: Double?
 }
 
 struct FeelsLike: Codable {
-    var day: Double
-    var night: Double
-    var eve: Double
-    var morn: Double
+    var day: Double?
+    var night: Double?
+    var eve: Double?
+    var morn: Double?
 }
